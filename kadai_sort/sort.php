cc59d3b2282e7c27a4760a63be4329b29c7a95ba
okawara01 <kosyan.n.project@gmail.com>
2025-05-23 10:54:36 +0900

    kadai_sortをアップロード

---
+++ kadai_sort/sort.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編</title>
+</head>
+
+<body>
+    <p>
+        <?php
+        function sort_2way($nums,$order) {
+    
+        if ( $order ) {
+           echo "昇順にソートします。<br>";
+           sort($nums);
+        }else {
+          echo "降順にソートします。<br>";
+          rsort($nums);
+        }
+        foreach ($nums as $num) {
+          echo $num . '<br>';
+        }
+        }
+
+        $array = [15, 4, 18, 23, 10];
+
+        sort_2way($array,true);
+        sort_2way($array,false);
+        ?>
+    </p>
+</body>
+
+</html>
